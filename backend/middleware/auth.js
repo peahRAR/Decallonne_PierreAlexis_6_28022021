463de2284f9e7105bc577fab321380dd373ae938
Pierre-Alexis DECALLONNE <peah@iMac-de-Pierre-Alexis.local>
2021-03-03 16:50:23 +0100

    Gestion de login et créer un compte terminé, voir a géré les autorisation plus tard

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -14,6 +14,6 @@ module.exports = (req, res, next) => {
            next(); 
         }
     } catch(error){
-        res.status(401).json({ error: error | 'Requête non authentifiée !'});
+        res.status(401).json({ message: error | 'Requête non authentifiée !'});
     }
 };
