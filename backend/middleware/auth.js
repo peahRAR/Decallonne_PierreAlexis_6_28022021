90d48bd014021b764f5e12104ed2310c06165f0c
Pierre-Alexis DECALLONNE <peah@iMac-de-Pierre-Alexis.local>
2021-03-03 16:47:45 +0100

    Gestion de login et créer un compte terminé, voir a géré les autorisation plus tard

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RAMDOM_TOKEN_SECRET'); // Changer la clé Token pour une plus longue en phase de prod
+        const decodedToken = jwt.verify(token, 'RAMDOM_TOKEN_SECRET'); // Changer la clé Token pour une plus longue, en phase de prod
         const userId = decodedToken.userId;
         if (req.body.userId && req.body.userId !== userId){
             throw 'User ID non valable';
